3c7fc9be8674c06ccd014afbba73b9225bd511ce
Vyacheslav Nedbaylo <arenda5000@gmail.com>
2024-03-27 23:23:24 +0200

    first commit

---
+++ HW1/index.js
@@ -0,0 +1,6 @@
+
+alert("Hello!");
+let name1 = prompt('What is your name?');
+let name2 = prompt('Where are you from?');
+alert("Nice to meet you, " + name1 + "!" + "  You are from " + name2 +"!");
+
