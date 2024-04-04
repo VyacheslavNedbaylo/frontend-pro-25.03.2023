28c9cf2ffcc76c7fe9c95cca34d20ccb0a0402e8
VyacheslavNedbaylo <151580883+VyacheslavNedbaylo@users.noreply.github.com>
2024-04-04 12:40:00 +0300

    Created HW2

---
+++ HW2/index.js
@@ -0,0 +1,12 @@
+
+let string1;
+let string2;
+let string3;
+
+string1 = prompt ("Введіть перше слово: ");
+string2 = prompt ("Введіть друге слово: ");
+string3 = prompt ("Введіть третє слово: ");
+
+
+console.log(string3 + string2 + string1 && string1 + string2 + string3 && string2 + string1 + string3);
+ 
