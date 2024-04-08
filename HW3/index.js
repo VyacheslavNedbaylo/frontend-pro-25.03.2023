666265daeac822cc7e864fdae59c7cf41e0af0d0
VyacheslavNedbaylo <151580883+VyacheslavNedbaylo@users.noreply.github.com>
2024-04-08 12:46:58 +0300

    added HW3

---
+++ HW3/index.js
@@ -0,0 +1,10 @@
+const string = "String";
+const number = 1;
+const boolean = true;
+const myUndefined = undefined;
+const symbol = Symbol("example");
+const bigInt = 1n;
+const object = Object();
+
+const message4 = `JS types:\n\n${typeof string}\n${typeof number}\n${typeof boolean}\n${null}\n${typeof myUndefined}\n${typeof symbol}\n${typeof bigInt}\n${typeof object}`;
+console.log(message4);
