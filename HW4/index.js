001c48ad517e770f35d52974ee98665f88d78712
VyacheslavNedbaylo <151580883+VyacheslavNedbaylo@users.noreply.github.com>
2024-04-08 12:57:19 +0300

    created HW4

---
+++ HW4/index.js
@@ -0,0 +1,12 @@
+const number1 = String(10369);
+const message1 =
+  number1[0] +
+  " " +
+  number1[1] +
+  " " +
+  number1[2] +
+  " " +
+  number1[3] +
+  " " +
+  number1[4];
+console.log(message1);
