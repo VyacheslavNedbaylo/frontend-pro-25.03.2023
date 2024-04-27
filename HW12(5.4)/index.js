c29043e7dcd3ef65b98c68605316cca27f7ec4f8
VyacheslavNedbaylo <151580883+VyacheslavNedbaylo@users.noreply.github.com>
2024-04-28 02:22:55 +0300

    Added HW12

---
+++ HW12(5.4)/index.js
@@ -0,0 +1,23 @@
+let shouldCheck = true;
+
+do {
+  const digit = prompt("Enter your digit");
+  let isPrime = true;
+
+  if (digit && Number.parseInt(digit)) {
+    for (let i = 2; i <= Math.sqrt(Number.parseInt(digit)); i++) {
+      if (digit % i === 0) {
+        isPrime = false;
+      }
+    }
+  } else {
+    if (digit === null) break;
+
+    alert("You enter invalid digit");
+    continue;
+  }
+
+  alert(`Digit ${digit} is ${isPrime ? "prime" : "not prime"}`);
+
+  shouldCheck = confirm("Do you want to check another digit?");
+} while (shouldCheck);
